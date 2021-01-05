1012f23247aaf5f178fd8781d1af243d9da50516
Brian Buxton <bbux.dev@gmail.com>
2021-01-05 16:35:44 +0000

    Initial Driver and first go at README

---
+++ cli.py
@@ -0,0 +1,31 @@
+#!/bin/env python3
+import json
+import argparse
+from datamaker import Loader
+from datamaker import StdOutOutputer
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Run datamaker.')
+    parser.add_argument('-s', '--spec', required=True,
+                        help='Spec to Use')
+    parser.add_argument('-i', '--iterations', default=100, type=int,
+                        help='Number of Iterations to Execute')
+
+    args = parser.parse_args()
+
+    with open(args.spec, 'r') as handle:
+        spec = json.load(handle)
+
+    loader = Loader(spec)
+    output = StdOutOutputer()
+    keys = [key for key in spec.keys() if key != 'refs']
+
+    for i in range(0, args.iterations):
+        for key in keys:
+            value = loader.get(key).next(i)
+            output.handle(key, value)
+
+
+if __name__ == '__main__':
+    main()
