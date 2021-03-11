8fc78adfb5c37f72543a5ba106592fc8b954da9a
Brian Buxton <bbux.dev@gmail.com>
2021-03-11 10:58:51 +0000

    Moved test cases to external file
    Added schema and tests for range type

--- schema/test_schemas.py
+++ schema/test_schemas.py
@@ -6,61 +6,55 @@ from jsonschema.exceptions import ValidationError
 ASSUMED_VALID = "valid"
 ASSUMED_INVALID = "invalid"
 
-tests = {
-    "combine.schema.json": {
-        ASSUMED_VALID: [
-            {"type": "combine", "refs": ["one", "two"]},
-            {"type": "combine", "fields": ["one", "two"]},
-            {"type": "combine", "refs": ["one", "two"], "config": {"join_with": " "}},
-            {"type": "combine", "refs": ["one", "two"], "config": {"as_list": True}},
-            {"type": "combine", "refs": ["one", "two"], "config": {"as_list": "true"}},
-            {"type": "combine", "refs": ["one", "two"], "config": {"as_list": "yes"}},
-            {"type": "combine", "refs": ["one", "two"], "config": {"as_list": "on"}},
-        ],
-        ASSUMED_INVALID: [
-            {},  # empty
-            {"type": "cobine"},   # misspelled
-            {"type": "combine"},  # no fields or refs
-            {"type": "combine", "refs": ["one"]},    # only one ref
-            {"type": "combine", "fields": ["one"]},  # only one field
-            {"type": "combine", "refs": ["one", "two"], "config": {"join_with": True}},  # invalid join_with param
-            {"type": "combine", "refs": ["one", "two"], "config": {"as_list": "tru"}},   # invalid as_list param
-        ]
-    },
-    "values.schema.json": {
-        ASSUMED_VALID: [
-            {"type": "values", "data": "constant"},
-            {"type": "values", "data": ""},  # blank string still a constant
-            {"type": "values", "data": 1},
-            {"type": "values", "data": 1.5},
-            {"type": "values", "data": True},
-            {"type": "values", "data": ["one", "two"]},
-            {"type": "values", "data": {"one": 0.4, "two": 0.5}},
-        ],
-        ASSUMED_INVALID: [
-            {},  # empty
-            {"type": "value"},   # misspelled
-            {"type": "values"},  # no fields or refs
-            {"type": "values", "test": []},  # empty data
-            {"type": "values", "data": []},  # empty data
-            {"type": "values", "data": {}},  # empty data
-        ]
-    }
-}
-
 
 def test_run_validation():
-    with open('definitions.json', 'r') as handle:
-        definitions = json.load(handle)
-
+    definitions = load_json('definitions.json')
+    tests = load_json('schema.tests.json')
+    should_have_failed = {}
     for file, type_tests in tests.items():
-        with open(file, 'r') as handle:
-            schema = json.load(handle)
-            # hack for now
-            schema['definitions'] = definitions['definitions']
+        schema = load_json(file)
+        # hack for now
+        schema['definitions'] = definitions['definitions']
         for should_be_valid in type_tests[ASSUMED_VALID]:
-            validate(should_be_valid, schema=schema)
+            validate(should_be_valid['test'], schema=schema)
         for should_not_be_valid in type_tests[ASSUMED_INVALID]:
-            #print(json.dumps(should_not_be_valid))
-            with pytest.raises(ValidationError) as err_info:
-                validate(should_not_be_valid, schema=schema)
+            # print(json.dumps(should_not_be_valid))
+            try:
+                validate(should_not_be_valid['test'], schema=schema)
+            except ValidationError:
+                continue
+            failed_for_file = should_have_failed.get(file)
+            if failed_for_file is None:
+                failed_for_file = []
+            failed_for_file.append(should_not_be_valid)
+            should_have_failed[file] = failed_for_file
+    for file, failed_for_file in should_have_failed.items():
+        print(f'Should have failed but did not for {file}:')
+        for should_not_be_valid in failed_for_file:
+            if 'msg' in should_not_be_valid:
+                print(should_not_be_valid['msg'])
+            print(should_not_be_valid['test'])
+    if len(should_have_failed) > 0:
+        pytest.fail('Some invalid specs did not fail validation')
+
+def test_validate_count_formats():
+    definitions = load_json('definitions.json')
+    schema = definitions['definitions']['count']
+
+    bad_counts = [
+        True,  # invalid count type,
+        [],  # empty count type,
+        [1.1, 1.7],  # counts not integers
+        {},  # empty count type
+        {"one": 0.7, "two": 0.3},  # bad count keys
+    ]
+
+    for bad_count in bad_counts:
+        # print(json.dumps(bad_count))
+        with pytest.raises(ValidationError):
+            validate(bad_count, schema=schema)
+
+
+def load_json(file_path):
+    with open(file_path, 'r') as handle:
+        return json.load(handle)
