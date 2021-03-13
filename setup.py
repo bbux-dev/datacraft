960f5d80b60dae741256f99bc4f74d402d2258f0
bbux-dev <77225624+bbux-dev@users.noreply.github.com>
2021-03-13 18:35:02 +0000

    Travis CI Integration (#15)
    Added .travis-ci.yml for continuous integration
    
    * Added explicit importlib-metadata dependency
    * Removed python 3.6 from supported versions
    importlib module not present till 3.7, which is needed for custom code
    loading
    * Attempt to add code coverage
    * Attempt to generate coverage report
    * Need pytest-cov dependency
    * Update setup.py

--- setup.py
+++ setup.py
@@ -33,11 +33,8 @@ setup(
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
 )
