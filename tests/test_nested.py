d8597501cf7a57776fcedbea0ad2b8838b959519
Brian Buxton <bbux.dev@gmail.com>
2021-07-20 11:31:09 +0100

    More tests for nested types count edge cases

--- tests/test_nested.py
+++ tests/test_nested.py
@@ -1,3 +1,4 @@
+import pytest
 from dataspec import builder, Loader
 from dataspec.supplier.core import nested
 
@@ -62,3 +63,21 @@ def test_single_nested_as_list():
     first = supplier.next(0)
     assert isinstance(first, list)
     assert list(first[0].keys()) == ['place_id', 'coordinates']
+
+
+nested_count_examples = [
+    (0, True, []),
+    (0, False, None),
+    (1, True, [{'inner': 'a'}]),
+    (1, False, {'inner': 'a'}),
+    (2, True, [{'inner': 'a'}, {'inner': 'b'}])
+]
+
+
+@pytest.mark.parametrize("count, as_list, expected", nested_count_examples)
+def test_nested_count_edge_cases(count, as_list, expected):
+    inner_spec = builder.spec_builder().values("inner", ["a", "b", "c"]).to_spec()
+    spec = builder.spec_builder().nested("outer", inner_spec, count=count, as_list=as_list).to_spec()
+    generator = spec.generator(1)
+    single_record = next(generator)
+    assert single_record['outer'] == expected
