4a42ee38c0dbcb5fbd16343a00d539c2e5d4454b
Brian Buxton <bbux.dev@gmail.com>
2023-09-17 16:44:14 -0700

    More thorough working inference before refactor

--- datacraft/infer.py
+++ datacraft/infer.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from collections import Counter
 from typing import Any, Dict, Generator, List, Union, Callable
 
@@ -125,10 +126,13 @@ class _Tree:
 
         data = {}
         for key, child in node.children.items():
-            data[key] = {
-                "type": "nested",
-                "fields": self.to_dict(child, func)
-            }
+            if child.is_leaf():
+                data[key] = self.to_spec(child, func)
+            else:
+                data[key] = {
+                    "type": "nested",
+                    "fields": self.to_spec(child, func)
+                }
 
         return data
 
@@ -136,7 +140,7 @@ class _Tree:
 def _process_jsons(jsons: List[Dict[str, Any]],
                    func: Union[Callable, None] = None) -> dict:
     """
-    Process a list of JSON data, print the leaf nodes, and return a JSON representation of the tree.
+    Process a list of JSON data, print the leaf nodes, and return a Data Spec of the tree.
 
     Args:
         jsons (List[Dict[str, Any]]): List of JSON data to process.
@@ -145,7 +149,7 @@ def _process_jsons(jsons: List[Dict[str, Any]],
                                    leaf node before they are returned.
 
     Returns:
-        str: JSON representation of the tree.
+        str: Data Spec from the tree.
     """
     tree = _Tree()
     for data in jsons:
@@ -162,34 +166,16 @@ def _substitute(values):
     return [_LOOKUP.get(v, v) for v in values]
 
 
-def compute_spec(values: List[Any]):
-    """
-    Compute spec from list of values
-    Args:
-        values:
-
-    Returns:
-        Dict[str, Any]: A dictionary with the inferred spec for the values
-    """
-    if _is_numeric(values):
-        return _compute_range(values)
-    if _requires_substitution(values):
-        values = _substitute(values)
-    # unique values, just rotate through them
-    if _are_values_unique(values):
-        return {
-            "type": "values",
-            "data": values
-        }
-    # use weighted values
-    return {
-        "type": "values",
-        "data": _calculate_weights(values)
-    }
-
-
 def _is_numeric(values):
     return all((isinstance(value, (int, float)) and not isinstance(value, bool)) for value in values)
+
+
+def _all_lists_numeric(values):
+    return all(_is_numeric(sublist) for sublist in values)
+
+
+def _all_lists_of_type(lists, type_check):
+    return all(isinstance(val, type_check) for sublist in lists for val in sublist)
 
 
 def _is_boolean(values):
@@ -227,6 +213,79 @@ def _compute_range(values: List[Union[int, float]]) -> Dict[str, Any]:
     }
 
 
+def _compute_list_range(values: list) -> dict:
+    """
+    Creates rand_range spec for lists of values
+    Args:
+        values: list of lists of values
+
+    Returns:
+        (dict): rand_range or rand_int_range spec
+    """
+    max_value = max(num for sublist in values for num in sublist)
+    min_value = min(num for sublist in values for num in sublist)
+
+    weights = _caclulate_list_size_weights(values)
+
+    if _all_lists_of_type(values, int):
+        field_type = "rand_int_range"
+    else:
+        field_type = "rand_range"
+
+    return {
+        "type": field_type,
+        "data": [min_value, max_value],
+        "config": {"count": weights, "as_list": True}
+    }
+
+
+def _caclulate_list_size_weights(values):
+    # Calculate the frequency of each sublist length
+    length_freq = Counter(len(sublist) for sublist in values)
+    # Calculate the total number of lists
+    total_lists = len(values)
+    # Calculate the weights for each sublist length
+    weights = {str(length): freq / total_lists for length, freq in length_freq.items()}
+    return weights
+
+
+def _compute_str_list_spec(values: list) -> dict:
+    """
+    Creates string spec from lists of list of strings
+    Args:
+        values: list of lists of strings
+
+    Returns:
+        (dict): with appropriate string spec
+    """
+
+    weights = _caclulate_list_size_weights(values)
+    # Flatten the list of lists
+    flat_list = [s for sublist in values for s in sublist]
+
+    # Determine the number of unique strings
+    unique_strings = set(flat_list)
+    num_unique = len(unique_strings)
+
+    # Determine the range of sizes of the strings
+    sizes = [len(s) for s in unique_strings]
+    min_size = min(sizes)
+    max_size = max(sizes)
+    avg_size = sum(sizes) / num_unique
+
+    # Count occurrences of each string
+    counts = Counter(flat_list)
+
+    # Filter and count the strings that appear more than once
+    repeated_count = sum(1 for count in counts.values() if count > 1)
+
+    return {
+        "type": "values",
+        "data": flat_list,
+        "config": {"count": weights, "as_list": True}
+    }
+
+
 def _calculate_weights(values: List[str]) -> Dict[str, float]:
     """
     Calculate the weights of occurrences of values from a list.
@@ -257,6 +316,49 @@ def _are_values_unique(values: List) -> bool:
     return len(values) == len(set(values))
 
 
+def _all_list_is_str(lists):
+    return all(isinstance(val, str) for sublist in lists for val in sublist)
+
+
+def compute_spec(values: List[Any]):
+    """
+    Compute spec from list of values
+    Args:
+        values: List of values accumulated for leaf node
+
+    Returns:
+        Dict[str, Any]: A dictionary with the inferred spec for the values
+    """
+    # insert mechanism here to elegantly handle all the types of lists of values
+    # there shouldn't be
+    # handle leaf nodes that are lists
+    if isinstance(values, list) and isinstance(values[0], list):
+        if _is_numeric(values[0]):
+            return _compute_list_range(values)
+        if _all_list_is_str(values):
+            return _compute_str_list_spec(values)
+        return {
+            "type": "values",
+            "data": values
+        }
+    if len(set(values)) > 1 and _is_numeric(values):
+        return _compute_range(values)
+    if _requires_substitution(values):
+        values = _substitute(values)
+
+    # unique values, just rotate through them
+    if _are_values_unique(values):
+        return {
+            "type": "values",
+            "data": values
+        }
+    # use weighted values
+    return {
+        "type": "values",
+        "data": _calculate_weights(values)
+    }
+
+
 def from_examples(examples: List[dict]) -> dict:
     """
     Generates a Data Spec from the list of example JSON records
@@ -276,4 +378,27 @@ def from_examples(examples: List[dict]) -> dict:
         >>> infer.from_examples(xmpls)
         {'foo': {'type': 'nested', 'fields': {'bar': {'type': 'rand_range', 'data': [22.3, 44.5]}, 'baz': {'type': 'values', 'data': ['single', 'double']}}}}
     """
+    if examples is None or len(examples) == 0:
+        return {}
     return _process_jsons(examples, func=compute_spec)
+
+
+def csv_to_spec(file_path: str) -> Dict[str, Union[str, Dict]]:
+    """
+    Read a CSV from the provided file path, convert it to JSON records,
+    and then pass it to the from_examples function to get the spec.
+
+    Args:
+        file_path (str): The path to the CSV file.
+
+    Returns:
+        Dict[str, Union[str, Dict]]: The inferred data spec from the CSV data.
+    """
+    # Read CSV using pandas
+    df = pd.read_csv(file_path)
+
+    # Convert DataFrame to list of JSON records
+    json_records = df.to_dict(orient='records')
+
+    # Get the spec using from_examples function
+    return from_examples(json_records)
--- tests/test_infer.py
+++ tests/test_infer.py
@@ -1,7 +1,72 @@
+import json
+
 import pytest
 
-from datacraft.infer import (from_examples, _is_numeric, _calculate_weights,
+from datacraft.infer import (from_examples, csv_to_spec, _is_numeric, _calculate_weights,
                              _are_values_unique, _compute_range)
+
+EXAMPLES = [
+    # 0. Empty List
+    ([], {}),
+
+    # 1. Single Key-Value Pair
+    ([{"key": "value"}], {"key": {"type": "values", "data": ["value"]}}),
+
+    # 2. Different Data Types
+    ([{"key1": 123, "key2": "string", "key3": 12.34, "key4": True, "key5": None}],
+     {"key1": {"type": "values", "data": [123]},
+      "key2": {"type": "values", "data": ["string"]},
+      "key3": {"type": "values", "data": [12.34]},
+      "key4": {"type": "values", "data": ["_TRUE_"]},
+      "key5": {"type": "values", "data": ["_NONE_"]}}),
+
+    # 3. Nested JSON
+    ([{"parent": {"child1": "value1", "child2": "value2"}}],
+     {"parent": {"type": "nested", "fields": {"child1": {"type": "values", "data": ["value1"]},
+                                              "child2": {"type": "values", "data": ["value2"]}}}}),
+
+    # 4. Lists in JSON
+    ([{"key": [1, 2, 3]}],
+     {'key': {'config': {'as_list': True, 'count': {'3': 1.0}}, 'data': [1, 3], 'type': 'rand_int_range'}}),
+
+    # 5. Mixed Data in Lists (this one's tricky; adjust according to your function's behavior)
+    ([{"key": [1, "two", 3.0]}], {"key": {"type": "values", "data": [[1, "two", 3.0]]}}),
+
+    # 6. Varying Nested Depth
+    ([{"key1": {"key2": {"key3": {"key4": "value"}}}}],
+     {"key1": {"type": "nested", "fields": {"key2": {"type": "nested", "fields": {
+         "key3": {"type": "nested", "fields": {"key4": {"type": "values", "data": ["value"]}}}}}}}}),
+
+    # 7. Inconsistent Data Types (assuming your function captures both data types)
+    ([{"key": "value"}, {"key": 123}], {"key": {"type": "values", "data": ["value", 123]}}),
+
+    # 8. Boolean and None Handling
+    ([{"key1": True, "key2": False, "key3": None}],
+     {"key1": {"type": "values", "data": ["_TRUE_"]},
+      "key2": {"type": "values", "data": ["_FALSE_"]},
+      "key3": {"type": "values", "data": ["_NONE_"]}}),
+
+    # 9. Larger Lists
+    ([{"key": list(range(100))}], {'key': {'type': 'rand_int_range',
+                                           'data': [0, 99],
+                                           'config': {'as_list': True, 'count': {'100': 1.0}}
+                                           }}),
+
+    # 10. Overlapping and Non-overlapping Keys
+    ([{"key1": "value1"}, {"key2": "value2"}],
+     {"key1": {"type": "values", "data": ["value1"]}, "key2": {"type": "values", "data": ["value2"]}}),
+
+    # 11. Special Characters in Keys
+    ([{"key with space": "value", "key_with_underscore": 123, "!@#$%^&*()": "special"}],
+     {"key with space": {"type": "values", "data": ["value"]},
+      "key_with_underscore": {"type": "values", "data": [123]},
+      "!@#$%^&*()": {"type": "values", "data": ["special"]}}),
+
+    # 12. Very Large Numbers and Small Floats
+    ([{"key1": 12345678901234567890, "key2": 0.000000123456}],
+     {"key1": {"type": "values", "data": [12345678901234567890]},
+      "key2": {"type": "values", "data": [0.000000123456]}})
+]
 
 
 def test_is_numeric():
@@ -38,7 +103,19 @@ def test_compute_float_range():
     assert _compute_range(values) == expected
 
 
-def test_from_examples():
+def test_from_example_simple():
+    examples = [
+        {"bar": 22.3, "baz": "single"},
+        {"bar": 44.5, "baz": "double"}
+    ]
+    expected = {
+        'bar': {'type': 'rand_range', 'data': [22.3, 44.5]},
+        'baz': {'type': 'values', 'data': ['single', 'double']}
+    }
+    assert from_examples(examples) == expected
+
+
+def test_from_examples_nested():
     examples = [
         {"foo": {"bar": 22.3, "baz": "single"}},
         {"foo": {"bar": 44.5, "baz": "double"}}
@@ -53,3 +130,63 @@ def test_from_examples():
         }
     }
     assert from_examples(examples) == expected
+
+
+def test_from_examples_list_value():
+    examples = [
+        {"bar": 22.3, "baz": [1, 2]},
+        {"bar": 44.5, "baz": [3, 4, 5]}
+    ]
+    expected = {
+        'bar': {'type': 'rand_range', 'data': [22.3, 44.5]},
+        'baz': {
+            'type': 'rand_int_range',
+            'data': [1, 5],
+            'config': {
+                'count': {'2': 0.5, '3': 0.5},
+                'as_list': True
+            }
+        }
+    }
+    assert from_examples(examples) == expected
+
+
+@pytest.fixture
+def sample_csv_file(tmpdir):
+    data = """foo,bar,baz
+1,22.3,single
+2,44.5,double
+3,44.6,triple"""
+
+    file_path = tmpdir.join("sample.csv")
+    with open(file_path, 'w') as f:
+        f.write(data)
+    return file_path
+
+
+def test_csv_to_spec(sample_csv_file):
+    result = csv_to_spec(sample_csv_file)
+
+    expected = {
+        'bar': {
+            'type': 'rand_range',
+            'data': [22.3, 44.6]
+        },
+        'baz': {
+            'type': 'values',
+            'data': ['single', 'double', 'triple']
+        },
+        'foo': {
+            'type': 'rand_int_range',
+            'data': [1, 3]
+        }
+    }
+
+    assert result == expected
+
+
+@pytest.mark.parametrize("input_data, expected_output", EXAMPLES)
+def test_from_examples(input_data, expected_output):
+    actual = from_examples(input_data)
+    assert actual == expected_output, f"Actual not equal to expected: {json.dumps(actual, indent=2)}, " \
+                                      f"{json.dumps(expected_output, indent=2)}"
