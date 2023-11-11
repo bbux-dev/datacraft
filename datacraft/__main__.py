7ff40f68184b22bf0215bb59e465c1e2c0cc7d6e
Brian Buxton <bbux.dev@gmail.com>
2023-11-10 16:41:37 -0800

    Added --endpoint-spec argument
    This specifies the path to a spec like file where each key
    is an endpoint and each value is the spec to generate data for
    that endpoint

--- datacraft/__main__.py
+++ datacraft/__main__.py
@@ -2,6 +2,8 @@
 """
 Entry point for datacraft tool
 """
+import json
+import os.path
 import sys
 
 from . import cli, suppliers
@@ -26,27 +28,43 @@ def main(argv):
     """Runs the tool """
 
     args = cli.parseargs(argv)
+    cli.configure_logging(args)
+    if args.server or args.endpoint_spec:
+        run_server(args)
+        return
+
     generator = cli.process_args(args)
     if generator is None:
         return
-    if args.server:
-        try:
-            from . import server
-            using_template_or_formatter = args.template or args.format
-            records_per_file = args.records_per_file
-            if records_per_file is None:
-                records_per_file = 1
-            server.run(generator,
-                       args.endpoint,
-                       args.port,
-                       data_is_json=(not using_template_or_formatter),
-                       count_supplier=suppliers.count_supplier(count=records_per_file),
-                       delay=args.server_delay)
-        except ModuleNotFoundError:
-            _log.warning('--server mode requires flask, pip/conda install flask and rerun command')
+
+    _log.info('Starting Processing...')
+    for _ in range(0, args.iterations):
+        # Generator will handle writing to configured output
+        next(generator)
+    _log.info('Finished Processing')
+
+
+def run_server(args):
+    if args.endpoint_spec:
+        if not os.path.exists(args.endpoint_spec):
+            raise FileNotFoundError("No file at path %s", args.endpoint_spec)
+        with open(args.endpoint_spec, "r", encoding="utf-8") as fp:
+            endpoints_spec = json.load(fp)
+        mappings = {endpoint: cli.generator_for_spec(args, spec) for endpoint, spec in endpoints_spec.items()}
     else:
-        _log.info('Starting Processing...')
-        for _ in range(0, args.iterations):
-            # Generator will handle using to configured output
-            next(generator)
-        _log.info('Finished Processing')
+        generator = cli.process_args(args)
+        mappings = {args.endpoint: generator}
+
+    try:
+        from . import server
+        using_template_or_formatter = args.template or args.format
+        records_per_file = args.records_per_file
+        if records_per_file is None:
+            records_per_file = 1
+        server.run(mappings,
+                   args.port,
+                   data_is_json=(not using_template_or_formatter),
+                   count_supplier=suppliers.count_supplier(count=records_per_file),
+                   delay=args.server_delay)
+    except ModuleNotFoundError:
+        _log.warning('--server mode requires flask, pip/conda install flask and rerun command')
--- datacraft/server.py
+++ datacraft/server.py
@@ -17,56 +17,62 @@ class _Server:
     """
 
     def __init__(self,
-                 generator: Generator,
-                 endpoint: str,
+                 endpoint_specs: dict,
                  port: int,
                  data_is_json: bool,
                  count_supplier: datacraft.ValueSupplierInterface,
                  delay: Union[float, None] = None):
-        self.generator = generator
-        self.endpoint = endpoint
+        self.endpoint_specs = endpoint_specs
         self.port = port
         self.data_is_json = data_is_json
         self.count_supplier = count_supplier
         self.delay = delay
         self.call_number = 0
 
-    def callback(self):
-        """
-        Callback for endpoint requests
+    def generate_view_func(self, generator):
+        """Generate a unique view function for a given endpoint configuration."""
 
-        Returns:
-            data as JSON or raw, until stop iteration, then 204 no more content
-        """
-        # next generated record
-        num_records = self.count_supplier.next(self.call_number)
-        try:
-            data = [next(self.generator) for _ in range(num_records)]
-        except StopIteration:
-            _log.warning('No more iterations available')
-            return flask.Response(None, status=204)
-        if self.delay:
-            time.sleep(self.delay)
-        if self.data_is_json:
+        def view_func():
+            """
+            Callback for endpoint requests
+
+            Returns:
+                data as JSON or raw, until stop iteration, then 204 no more content
+            """
+            # next generated record
+            num_records = self.count_supplier.next(self.call_number)
+            try:
+                data = [next(generator) for _ in range(num_records)]
+            except StopIteration:
+                _log.warning('No more iterations available')
+                return flask.Response(None, status=204)
+            if self.delay:
+                time.sleep(self.delay)
+            if self.data_is_json:
+                return flask.jsonify(data)
+            # this may already be json or templated data
+            if num_records == 1:
+                return data[0]
+            # cannot return list without jsonifying it
             return flask.jsonify(data)
-        # this may already be json or templated data
-        if num_records == 1:
-            return data[0]
-        # cannot return list without jsonifying it
-        return flask.jsonify(data)
+
+        return view_func
 
     def run(self):
         """ run the Flask app """
         app = flask.Flask(__name__)
-        _log.info('Adding endpoint to server: %s', self.endpoint)
-        app.add_url_rule(self.endpoint,
-                         view_func=self.callback,
-                         methods=['POST', 'GET'])
+        for endpoint_path, generator in self.endpoint_specs.items():
+            _log.info('Adding endpoint to server: %s', endpoint_path)
+            view_func = self.generate_view_func(generator)
+            app.add_url_rule(endpoint_path,
+                             endpoint=endpoint_path,
+                             view_func=view_func,
+                             methods=['POST', 'GET'])
+
         app.run(port=self.port)
 
 
-def run(generator: Generator,
-        endpoint: str,
+def run(endpoint_map: dict,
         port: int,
         data_is_json: bool,
         count_supplier: datacraft.ValueSupplierInterface,
@@ -76,12 +82,11 @@ def run(generator: Generator,
     the provided endpoint. End point should start with /. When StopIteration encountered, returns a 204 status code.
 
     Args:
-        generator: that provides response data as dictionary
-        endpoint: to serve data on i.e. /data
+        endpoint_map: endpoint to generator that provides response data as dictionary for that end point
         port: to serve data on e.g. 8080
         data_is_json: if the data should be returned as JSON, default is as string
         count_supplier: supplies the number of values to generate for each call
         delay: number of seconds to pause between request and response
     """
-    server = _Server(generator, endpoint, port, data_is_json, count_supplier, delay)
+    server = _Server(endpoint_map, port, data_is_json, count_supplier, delay)
     server.run()
